7b62f7d3331eaab2912f82ef831d56cb0a1362fe
Zafar Takhirov <z.tahirov@gmail.com>
2014-05-08 15:58:39 +0000

    index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require('express')
 var app = express();
 
+var buffer = fs.readFileSync('index.html', function(err, data) {
+    if (err) throw err;
+    console.log(data);
+}
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+
+
 
 app.get('/', function(request, response) {
   response.send('Hello World 2!')
