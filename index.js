7a9ce1f86b49bdb2445858d6e1bb42fac5bc41e7
Zafar Takhirov <z.tahirov@gmail.com>
2014-05-09 08:20:36 +0000

    AUTOCOMMIT: Fri May  9 08:20:36 UTC 2014

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 var fs = require ('fs');
 var express = require('express')
 var app = express();
-var content = "";
 
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-fs.readFileSync("index.html", function (err, data) {
-    if (err) throw err;
-    // console.log("debug");
-    // console.log(data);
-    content = data;
-});
+var buffer = fs.readFileSync("index.html");
+
 
 
 app.get('/', function(request, response) {
     // response.send('Hello World 2!');
-    response.send(content.toString);
+    response.send(buffer.toString());
 })
 
 app.listen(app.get('port'), function() {
