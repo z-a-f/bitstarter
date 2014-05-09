7367096f8869c13a84c43abb77ca5c9459002b47
Zafar Takhirov <z.tahirov@gmail.com>
2014-05-09 05:44:37 +0000

    test

--- index.js
+++ index.js
@@ -2,25 +2,21 @@ var fs = require ('fs');
 var express = require('express')
 var app = express();
 
-var buffer = fs.readFileSync('index.html', function (err, data) {
-  if (err){
-      throw err;
-      console.log("debug");
-  }
-  console.log(data);
-});
-
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var buffer = fs.readFileSync('index.html', function (err, data) {
+    if (err) throw err;
+    console.log("debug");
+    console.log(data);
+});
 
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-  buffer; 
-  // response.send(buffer.toString);
+    response.send('Hello World 2!');
+    // response.send(buffer.toString);
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+    console.log("Node app is running at localhost:" + app.get('port'))
 })
