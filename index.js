16229eb006f08242181369d0d77e83d149b45e8e
Zafar Takhirov <z.tahirov@gmail.com>
2014-05-09 05:19:56 +0000

    index.js modified

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express')
 var app = express();
 
-var buffer = fs.readFileSync('index.html', function(err, data) {
-    if (err) throw err;
-    console.log(data);
-}
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
