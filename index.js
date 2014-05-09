119bdd0b84bf8f60b3bdc25d01b0230935868211
Zafar Takhirov <z.tahirov@gmail.com>
2014-05-09 05:27:23 +0000

    suck

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var express = require('express')
 var app = express();
-/*
-var buffer = fs.readFile('index.html', function (err, data) {
+
+var buffer = fs.readFileSync('index.html', function (err, data) {
   if (err) throw err;
   console.log(data);
 });
-*/
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
-  // response.send(buffer.toString);
+  // response.send('Hello World 2!')
+  response.send(buffer.toString);
 })
 
 app.listen(app.get('port'), function() {
