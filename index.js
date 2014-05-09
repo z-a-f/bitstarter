7a709b9438a87a08457975d4c7ab7351e8039ec0
Zafar Takhirov <z.tahirov@gmail.com>
2014-05-09 05:31:43 +0000

    suck that

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 
 var buffer = fs.readFileSync('index.html', function (err, data) {
   if (err) throw err;
+  console.log("debug");
   console.log(data);
 });
 
@@ -12,8 +13,8 @@ app.use(express.static(__dirname + '/public'))
 
 
 app.get('/', function(request, response) {
-  // response.send('Hello World 2!')
-  response.send(buffer.toString);
+  response.send('Hello World 2!')
+  // response.send(buffer.toString);
 })
 
 app.listen(app.get('port'), function() {
