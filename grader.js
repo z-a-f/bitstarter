64babf6a0a80994a5afb5705e5d8f0c26524c4d9
Zafar Takhirov <z.tahirov@gmail.com>
2014-05-10 22:52:34 +0000

    Small modification

--- grader.js
+++ grader.js
@@ -1,27 +1,27 @@
 #!/usr/bin/env node
-   /*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
+/*
+  Automatically grade files for the presence of specified HTML tags/attributes.
+  Uses commander.js and cheerio. Teaches command line application development
+  and basic DOM parsing.
 
-References:
+  References:
 
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
+  + cheerio
+  - https://github.com/MatthewMueller/cheerio
+  - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+  - http://maxogden.com/scraping-with-node.html
 
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+  + commander.js
+  - https://github.com/visionmedia/commander.js
+  - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
 
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-   */
+  + JSON
+  - http://en.wikipedia.org/wiki/JSON
+  - https://developer.mozilla.org/en-US/docs/JSON
+  - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+*/
 
-    var fs = require('fs');
+var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
