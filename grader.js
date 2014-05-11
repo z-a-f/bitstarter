59fa3b9a6ca8c5419b0c05ed6298bdb03496509d
Zafar Takhirov <z.tahirov@gmail.com>
2014-05-11 03:19:13 +0000

    Final version

--- grader.js
+++ grader.js
@@ -20,16 +20,15 @@
   - https://developer.mozilla.org/en-US/docs/JSON
   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
-var sys = require('util');
-var rest = require('restler');
-var fs = require('fs');
-var program = require('commander');
-var cheerio = require('cheerio');
-var HTMLFILE_DEFAULT = "index.html";
-var HTMLFILE_DEFAULT = null;
-var CHECKSFILE_DEFAULT = "checks.json";
 
-var assertFileExists = function(infile) {
+var rest = require('restler'); // Restler for URL fetching
+var fs = require('fs'); // Filesystem for file opening
+var program = require('commander'); // Node Commander for command line framework
+var cheerio = require('cheerio'); // Node Cheerio for jQuerry
+var HTMLFILE_DEFAULT = "index.html"; // This is the page we look for by default
+var CHECKSFILE_DEFAULT = "checks.json"; // This is the default checking dictionary
+
+var assertFileExists = function(infile) { // Assert the file exists :)
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
@@ -38,14 +37,24 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile).toString());
-};
-
+/**
+ * Load the check dictionary
+ **/
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
+/**
+ * Load the HTML file using Cheerio
+ * Note: It looks like Cheerio cannot process buffers, need top convert to string
+ **/
+var cheerioHtmlFile = function(htmlfile) {
+    return cheerio.load(fs.readFileSync(htmlfile).toString());
+};
+
+/**
+ * File checker
+ **/
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
@@ -57,6 +66,10 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+/**
+ * URL checker
+ * loads the HTML from an URL and checks it using the loadChecks function
+ **/
 var checkURL = function(url, checksfile){
     $ = cheerio.load(url);
     var checks = loadChecks(checksfile).sort();
@@ -68,36 +81,38 @@ var checkURL = function(url, checksfile){
     return out;
 };
 
-
+/**
+ * Workaround for commander.js issue.
+ * http://stackoverflow.com/a/6772648
+ **/
 var clone = function(fn) {
-    // Workaround for commander.js issue.
-    // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+/**
+ * Main part
+ **/
 if(require.main == module) {
-    program
+    program // See Commander documentation for details
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
 	.option('-u, --url <url>', 'Check URL')
         .parse(process.argv);
 
-    if(program.url != null){
+    if(program.url != null){ // Is it a URL?
 	console.log("URL: ", program.url);
         rest.get(program.url).on('complete', function(result) {
 	    if(result instanceof Error){
 		console.error('Error: ' + result.message);
 		this.retry(1000);
-	    }else {
-		//sys.puts(result); //prints JS of file URL
+	    }else { // If no error process using the above functions:
 		var checkJson = checkURL(result, program.checks);
 		var outJson = JSON.stringify(checkJson, null, 4);
                 console.log("url check output: ", outJson);
-		// fs.writeFileSync("./hw3p3.json", outJson);  //write result to JSON file
 	    }
 	});
     }
-    else{
+    else{ // Is it a FILE?
         console.log("FILE: ", program.file);
 	var checkJson = checkHtmlFile(program.file, program.checks);
 	var outJson = JSON.stringify(checkJson, null, 4);
