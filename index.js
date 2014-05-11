1f6b9bff90523ebe205018588b399df35443fb31
Zafar Takhirov <z.tahirov@gmail.com>
2014-05-11 23:09:21 +0000

    Listen to 8080

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require('express')
 var app = express();
 
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
 var buffer = fs.readFileSync("index.html");
