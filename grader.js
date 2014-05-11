2c94126208f00fa83ec89e545a3b440dfa56abca
Zafar Takhirov <z.tahirov@gmail.com>
2014-05-11 00:14:44 +0000

    Modified grader

--- grader.js
+++ grader.js
@@ -1,24 +1,24 @@
 #!/usr/bin/env node
 /*
-  Automatically grade files for the presence of specified HTML tags/attributes.
-  Uses commander.js and cheerio. Teaches command line application development
-  and basic DOM parsing.
+Automatically grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teaches command line application development
+and basic DOM parsing.
 
-  References:
+References:
 
-  + cheerio
-  - https://github.com/MatthewMueller/cheerio
-  - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-  - http://maxogden.com/scraping-with-node.html
+ + cheerio
+   - https://github.com/MatthewMueller/cheerio
+   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+   - http://maxogden.com/scraping-with-node.html
 
-  + commander.js
-  - https://github.com/visionmedia/commander.js
-  - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
+ + commander.js
+   - https://github.com/visionmedia/commander.js
+   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
 
-  + JSON
-  - http://en.wikipedia.org/wiki/JSON
-  - https://developer.mozilla.org/en-US/docs/JSON
-  - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+ + JSON
+   - http://en.wikipedia.org/wiki/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON
+   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
 var fs = require('fs');
@@ -49,6 +49,7 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
+	console.log($("test"));
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
     }
@@ -63,8 +64,8 @@ var clone = function(fn) {
 
 if(require.main == module) {
     program
-        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
