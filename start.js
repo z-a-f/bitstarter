0bdd0a7428fbff1b7b0e1688c791bdfe25c7c9fb
Zafar Takhirov <z.tahirov@gmail.com>
2014-05-12 02:04:53 +0000

    AUTOCOMMIT: Mon May 12 02:04:53 UTC 2014

--- start.js
+++ start.js
@@ -6,7 +6,7 @@ var app = express();
 app.set('port', (process.env.PORT || 8080))
 app.use(express.static(__dirname + '/public'))
 
-var buffer = fs.readFileSync("index.html");
+var buffer = fs.readFileSync("./public/index.html");
 
 app.get('/', function(request, response) {
     // response.send('Hello World 2!');
